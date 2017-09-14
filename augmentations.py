9876555b3ca36c05602347fdb137791d8894fcb1
sid0710 <sid0710@live.com>
2017-09-14 10:58:51 +0530

    Updated augmentations to remove some dependencies
    Changed the random cropping function and the way the library does the reading and writing of the wav files.

--- augmentations.py
+++ augmentations.py
@@ -1,19 +1,39 @@
-#from pysndfx import AudioEffectsChain
-import essentia.standard as es
-import argparse
 import random
 import os
 import numpy as np
 import tempfile
 from os.path import basename
-import logging
 import subprocess
+from scipy.io.wavfile import read,write
+import math
 
-#TODO: Add support for output directory choice, currently it writes the output file in the same directory
+#Change below to keep or delete the files
+FILE_DELETION = False
+
+#Output files directory (create if not already present)
+outfile_path = "./output"
+if not os.path.exists(outfile_path):
+    os.makedirs(outfile_path)
 
 def tmp_path(ext=''):
     tf = tempfile.NamedTemporaryFile()
     return tf.name + ext
+
+#For reading wav files in mono
+def monoWavRead(filename):
+    fs, x = read(filename=filename)
+    if (x.ndim==1):
+        samples = x
+    else:
+        samples = x[:, 0]
+    return fs, samples
+
+def extractFeaturesAndDelete(filename):
+    #Extract features or whatever else you want to do with the created augmented file before deletion here
+
+    #Deletion
+    os.unlink(filename)
+    assert not os.path.exists(filename)
 
 def random_cropping(infile, minLength = 1):
     """ Cropping the infile with a minimum duration of minLength
@@ -22,23 +42,23 @@ def random_cropping(infile, minLength = 1):
         infile (str): Filename
         minLength (float) : Minimum duration for randomly cropped excerpt
     """
-    loader = es.MonoLoader(filename = infile)
-    x = loader()
-    fs = 44100
+    fs, x = monoWavRead(filename = infile)
     endTime = x.size * 1/fs
     if (endTime > minLength):
-        st = random.uniform (0.0, endTime - minLength)
+        st = random.uniform(0.0, endTime - minLength)
         end = random.uniform(st + minLength, endTime)
-        slicer = es.Slicer(startTimes = [st], endTimes = [end])
-        y = slicer(x)
+
+        y = x[int(math.floor(st*fs)):int(math.ceil(end*fs))]
+
         #Change the output file name to suit your requirements here
-        outfile_name = infile.split(".")[0] + ("_randomCropped%s.wav" % minLength)
-        #outfile = os.path.join(outfile_path, outfile_name)
-        writer = es.MonoWriter(filename = outfile_name)
-        writer(y[0])
+        outfile_name = os.path.basename(infile).split(".")[0] + ("_randomCropped%s.wav" % minLength)
+        outfile = os.path.join(outfile_path, outfile_name)
+        write(filename = outfile, rate = fs, data = y)
+        if (FILE_DELETION):
+            extractFeaturesAndDelete(outfile)
     else :
         print "MinLength provided is greater than the duration of the song."
-
+  
 def add_noise(infile, noise_name, snr):
     """ Add noise to infile
 
@@ -47,31 +67,28 @@ def add_noise(infile, noise_name, snr):
         noise_name (str): Name of noise (currently only 'white-noise')
         snr (float): SNR of output sound
     """
-    loader1 = es.MonoLoader(filename = infile)
-    x = loader1()
+    fs1, x = monoWavRead(filename=infile)
+
     noise_path = './sounds/%s.wav' % noise_name
-    loader2 = es.MonoLoader(filename = noise_path)
-    z = loader2()
+    fs2, z = monoWavRead(filename=noise_path)
+
     while z.shape[0] < x.shape[0]:
         z = np.concatenate((z, z), axis=0)
     z = z[0: x.shape[0]]
     rms_z = np.sqrt(np.mean(np.power(z, 2)))
-    logging.debug("rms_z: %f" % rms_z)
     rms_x = np.sqrt(np.mean(np.power(x, 2)))
-    logging.debug("rms_x: %f" % rms_x)
     snr_linear = 10 ** (snr / 20.0)
-    logging.debug("snr , snr_linear: %f, %f" % (snr, snr_linear))
     noise_factor = rms_x / rms_z / snr_linear
-    logging.debug("y = x  + z * %f" % noise_factor)
     y = x + z * noise_factor
     rms_y = np.sqrt(np.mean(np.power(y, 2)))
     y = y * rms_x / rms_y
 
     #Change the output file name to suit your requirements here
-    outfile_name = infile.split(".")[0] + ("_addedNoise%s.wav" % str(snr))
-    #outfile = os.path.join(outfile_path, outfile_name)
-    writer = es.MonoWriter(filename = outfile_name)
-    writer(y)
+    outfile_name = os.path.basename(infile).split(".")[0] + ("_addedNoise%s.wav" % str(snr))
+    outfile = os.path.join(outfile_path, outfile_name)
+    write(filename = outfile, rate = fs1, data = y)
+    if (FILE_DELETION):
+        extractFeaturesAndDelete(outfile)
 
 def convolve(infile, ir_name, level = 0.5):
     """ Apply convolution to infile using impulse response given
@@ -81,21 +98,22 @@ def convolve(infile, ir_name, level = 0.5):
         ir_name can be  'smartphone_mic' or 'classroom'
         level (float) : can be between 0 and 1, default value = 0.5
     """
-    loader1 = es.MonoLoader(filename = infile)
-    x = loader1()
-    logging.info('Convolving with %s and level %f' % (ir_name, level))
+    fs1, x = monoWavRead(filename=infile)
+
     x = np.copy(x)
     #Change the path below for the sounds folder
     ir_path = './sounds/ir_{0}.wav'.format(ir_name)
-    loader2 = es.MonoLoader(filename = ir_path)
-    ir = loader2()
+
+    fs2, ir = monoWavRead(filename=ir_path)
+
     y = np.convolve(x, ir, 'full')[0:x.shape[0]] * level + x * (1 - level)
 
     #Change the output file name to suit your requirements here
-    outfile_name = infile.split(".")[0] + ("_convolved%s.wav" % str(level))
-    #outfile = os.path.join(outfile_path, outfile_name)
-    writer = es.MonoWriter(filename = outfile_name)
-    writer(y)
+    outfile_name = os.path.basename(infile).split(".")[0] + ("{0}_convolved{1}.wav".format(ir_name, level))
+    outfile = os.path.join(outfile_path, outfile_name)
+    write(filename = outfile, rate = fs1, data = y)
+    if (FILE_DELETION):
+        extractFeaturesAndDelete(outfile)
 
 def apply_gain(infile, gain):
     """ Apply gain to infile
@@ -104,40 +122,35 @@ def apply_gain(infile, gain):
         infile (str): Filename
         gain (float) : gain in dB (both positive and negative)
     """
-    loader = es.MonoLoader(filename = infile)
-    x = loader()
-    logging.info("Apply gain %f dB" % gain)
+    fs1, x = monoWavRead(filename=infile)
+
     x = np.copy(x)
     x = x * (10 ** (gain / 20.0))
     x = np.minimum(np.maximum(-1.0, x), 1.0)
     #Change the output file name to suit your requirements here
-    outfile_name = infile.split(".")[0] + ("_gain%s.wav" % str(gain))
-    #outfile = os.path.join(outfile_path, outfile_name)
-    writer = es.MonoWriter(filename = outfile_name)
-    writer(x)
+    outfile_name = os.path.basename(infile).split(".")[0] + ("_gain%s.wav" % str(gain))
+    outfile = os.path.join(outfile_path, outfile_name)
+    write(filename = outfile, rate = fs1, data = x)
+    if (FILE_DELETION):
+        extractFeaturesAndDelete(outfile)
 
-def apply_rubberband(infile, time_stretching_ratio=1.0, pitch_shifting_ratio=1.0):
+def apply_rubberband(infile, time_stretching_ratio=1.0, pitch_shifting_semitones=1):
     """ Use rubberband tool to apply time stretching and pitch shifting
 
     Args:
         infile (str): Filename
-        time_stretching_ratio (float): Ratio of time stretching
-        pitch_shifting_ratio (float): Ratio of pitch shifting
+        time_stretching_ratio (float): Ratio of time stretching (-9.99 to 9.99)
+        pitch_shifting_semitones (int): Pitch shift in semitones
 
-        Ratios can be from -9.99 to 9.99
     """
-    loader1 = es.MonoLoader(filename = infile)
-    x = loader1()
-    logging.info("Applying rubberband. ts_ratio={0}, ps_ratio={1}".format(
-        time_stretching_ratio,
-        pitch_shifting_ratio))
+    fs1, x = monoWavRead(filename=infile)
+
     tmp_file_1 = tmp_path()
     tmp_file_2 = tmp_path()
-    writer = es.MonoWriter(filename = tmp_file_1)
-    writer(x)
-    cmd = "rubberband -c 1 -t {0} -f {1} {2} {3}".format(
+    write(filename = tmp_file_1, rate = fs1, data = x)
+    cmd = "rubberband -c 1 -t {0} -p {1} {2} {3}".format(
         time_stretching_ratio,
-        pitch_shifting_ratio,
+        pitch_shifting_semitones,
         tmp_file_1,
         tmp_file_2)
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
@@ -145,13 +158,15 @@ def apply_rubberband(infile, time_stretching_ratio=1.0, pitch_shifting_ratio=1.0
     out, err = p.communicate()
     if p.returncode != 0:
         print "ERROR!"
-    loader2 = es.MonoLoader(filename = tmp_file_2)
-    y = loader2()
+
+    fs2, y = monoWavRead(filename=tmp_file_2)
+
     #Change the output file name to suit your requirements here
-    outfile_name = infile.split(".")[0] + ("_timestr%s_pitchshift%s.wav" % (str(time_stretching_ratio),str(pitch_shifting_ratio)))
-    #outfile = os.path.join(outfile_path, outfile_name)
-    writer = es.MonoWriter(filename = outfile_name)
-    writer(y)
+    outfile_name = os.path.basename(infile).split(".")[0] + ("_timestr%s_pitchshift%s.wav" % (str(time_stretching_ratio),str(pitch_shifting_semitones)))
+    outfile = os.path.join(outfile_path, outfile_name)
+    write(filename = outfile, rate = fs1, data = y)
+    if (FILE_DELETION):
+        extractFeaturesAndDelete(outfile)
 
 def apply_dr_compression(infile, degree):
     """ Apply dynamic range compression using SOX
@@ -160,12 +175,12 @@ def apply_dr_compression(infile, degree):
         infile (str): Filename
         degree (int): Can be only 1, 2, 3
     """
-    loader1 = es.MonoLoader(filename = infile)
-    x = loader1()
+    fs1, x = monoWavRead(filename = infile)
+
     tmpfile_1 = tmp_path('.wav')
     tmpfile_2 = tmp_path('.wav')
-    writer = es.MonoWriter(filename = tmpfile_1)
-    writer(x)
+
+    write(filename = tmpfile_1, rate = fs1, data = x)
     if degree == 1:
         cmd = "sox {0} {1} compand 0.01,0.20 -40,-10,-30 5"
     elif degree == 2:
@@ -173,26 +188,26 @@ def apply_dr_compression(infile, degree):
     elif degree == 3:
         cmd = "sox {0} {1} compand 0.01,0.1 -70,-60,-70,-30,-70,0,-70 45"
     cmd = cmd.format(tmpfile_1, tmpfile_2)
-    logging.info(cmd)
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
     out, err = p.communicate()
     if p.returncode != 0:
         print "ERROR!"
-    loader2 = es.MonoLoader(filename = tmpfile_2)
-    y = loader2()
+    fs2, y = monoWavRead(filename = tmpfile_2)
+
     #Change the output file name to suit your requirements here
-    outfile_name = infile.split(".")[0] + ("_drCompression%s.wav" % str(degree))
-    #outfile = os.path.join(outfile_path, outfile_name)
-    writer = es.MonoWriter(filename = outfile_name)
-    writer(y)
+    outfile_name = os.path.basename(infile).split(".")[0] + ("_drCompression%s.wav" % str(degree))
+    outfile = os.path.join(outfile_path, outfile_name)
+    write(filename = outfile, rate = fs1, data = y)
+    if (FILE_DELETION):
+        extractFeaturesAndDelete(outfile)
 
 def apply_eq(infile, value):
     """ Applying equalizer effects using SOX
 
     Args:
         infile (str): Filename
-        value (three ';' separated values): freq_hz;bw_hz;gain_db (frequency/bandwidth/gain)
+        value (three ';' separated values): freq_hz;bw_hz;gain_db (frequency;bandwidth;gain)
         
     From the SOX documentation (http://sox.sourceforge.net/sox.html) :
     Apply a two-pole peaking equalisation (EQ) filter. With this filter, the signal-level at and around 
@@ -202,31 +217,29 @@ def apply_eq(infile, value):
     Beware of Clipping when using a positive gain
     """
 
-    loader1 = es.MonoLoader(filename = infile)
-    x = loader1()
+    fs1, x = monoWavRead(filename = infile)
     freq, bw, gain = map(int, value.split(';'))
-    logging.info("Equalizing. f=%f, bw=%f, gain=%f" % (freq, bw, gain))
+
     tmpfile_1 = tmp_path('.wav')
     tmpfile_2 = tmp_path('.wav')
-    writer = es.MonoWriter(filename = tmpfile_1)
-    writer(x)
+    write(filename = tmpfile_1, rate = fs1, data = x)
     cmd = "sox {0} {1} equalizer {2} {3} {4}".format(
         tmpfile_1,
         tmpfile_2,
         freq,
         bw,
         gain)
-    logging.info(cmd)
+
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
     out, err = p.communicate()
     if p.returncode != 0:
         print "ERROR!"
-    loader2 = es.MonoLoader(filename = tmpfile_2)
-    y = loader2()
+    fs2, y = monoWavRead(filename = tmpfile_2)
     #Change the output file name to suit your requirements here
-    outfile_name = infile.split(".")[0] + ("_eq%s.wav" % str(value))
-    #outfile = os.path.join(outfile_path, outfile_name)
-    writer = es.MonoWriter(filename = outfile_name)
-    writer(y)
+    outfile_name = os.path.basename(infile).split(".")[0] + ("_eq%s.wav" % str(value))
+    outfile = os.path.join(outfile_path, outfile_name)
+    write(filename = outfile, rate = fs1, data = y)
+    if (FILE_DELETION):
+        extractFeaturesAndDelete(outfile)
 
